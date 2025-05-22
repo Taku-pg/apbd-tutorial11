333acbda01ee83dd56b202d8aca40263ea01fd6f
Taku-pg <kaitsu505@gmail.com>
2025-05-22 08:42:19 +0200

    init

---
+++ Model/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace apbd_tutorial11.Model;
+[Table("Patient")]
+public class Patient
+{
+    [Key]
+    public int IdPatient { get; set; }
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+    [MaxLength(100)]
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    
+    public ICollection<Prescription> Prescriptions { get; set; }
+}
